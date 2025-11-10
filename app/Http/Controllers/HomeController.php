4da287cd44821c477efce54695ad3ccb92a59d58
Tortousa <danielltortosa@gmail.com>
2025-11-10 23:12:39 +0100

    Models

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Car;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index() {
+
+        //Selecciona todos los coches
+        $cars = Car::get();
+
         return 'Index';
     }
 }
