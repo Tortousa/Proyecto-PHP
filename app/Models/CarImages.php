45d03cc557664a5cc22ade5f2de2a1961a6417e5
Tortousa <danielltortosa@gmail.com>
2025-11-11 14:36:35 +0100

    Seeders y Models update

--- app/Models/CarImages.php
+++ app/Models/CarImages.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CarImages extends Model
 {
@@ -12,4 +13,9 @@ class CarImages extends Model
     public $timestamps = false;
 
     protected $fillable = ['image_path', 'positon'];
+
+    public function car(): BelongsTo
+    {
+        return $this->belongsTo(Car::class);
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\Car;
+use App\Models\CarImages;
+use App\Models\CarType;
+use App\Models\City;
+use App\Models\FuelType;
+use App\Models\Maker;
+use App\Models\Model;
+use App\Models\State;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,11 +24,83 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        CarType::factory()
+            ->sequence(
+                ['name' => 'Sedan',],
+                ['name' => 'Hatchback',],
+                ['name' => 'SUV',],
+                ['name' => 'Pickup Truck',],
+                ['name' => 'Minivan',],
+                ['name' => 'Jeep',],
+                ['name' => 'Coupe',],
+                ['name' => 'Crossover',],
+                ['name' => 'Sports Car',],
+            )
+            ->count(9)
+            ->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        FuelType::factory()
+            ->sequence(
+                ['name' => 'Gasoline',],
+                ['name' => 'Diesel',],
+                ['name' => 'Electric',],
+                ['name' => 'Hybrid',],
+            )
+            ->count(4)
+            ->create();
+
+        $states = [
+            'California' => ['Los Angeles', 'San Francisco', 'San Diego'],
+            'New York' => ['New York City', 'Albany', 'Buffalo'],
+        ];
+
+
+        foreach ($states as $state => $cities) {
+            State::factory()
+                ->state(['name' => $state])
+                ->has(
+                    City::factory()
+                    ->count(count($cities))
+                    ->sequence(...array_map(fn($city) => ['name' => $city], $cities))
+                )
+                ->create();
+        }
+
+        $makers = [
+            'Toyota' => ['Carmy', 'Corolla', 'RAV4'],
+            'Ford' => ['Escape', 'Mustang', 'F-150'],
+        ];
+
+
+        foreach ($makers as $maker => $models) {
+            Maker::factory()
+                ->state(['name' => $maker])
+                ->has(
+                    Model::factory()
+                        ->count(count($models))
+                        ->sequence(...array_map(fn($model) => ['name' => $model], $models))
+                )
+                ->create();
+        }
+
+        User::factory()
+            ->count(3)
+            ->create();
+
+        User::factory()
+            ->count(2)
+            ->has(
+                Car::factory()
+                ->count(50)
+                ->has(
+                    CarImages::factory()
+                    ->count(5)
+                    ->sequence(fn(Sequence $sequence) => ['position' => $sequence->index % 5 + 1]),
+                    'images'
+                )
+                ->hasFeatures(),
+                'favoritesCars'
+            )
+            ->create();
     }
 }
